42363a2681bf95895b954551bb8cc3fc8f7fbf48
Michael Gomulak <12242119+Eameija@users.noreply.github.com>
2021-08-09 16:18:35 +0200

    Render first image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+
+const WIDTH: u16 = 256;
+const HEIGHT: u16 = 256;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(r"./image.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, u32::from(WIDTH), u32::from(HEIGHT));
+    encoder.set_color(png::ColorType::RGBA);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+
+    let mut pixels: Vec<u8> = vec![];
+
+    for y in 0..HEIGHT {
+        println!("Remaining lines: {}", HEIGHT - y);
+        for x in 0..WIDTH {
+            let xi = x as f64 / (WIDTH - 1) as f64;
+            let yi = y as f64 / (HEIGHT - 1) as f64;
+
+            let red = (255.0 * xi) as u8;
+            let green = (255.0 * yi) as u8;
+
+            pixels.push(red);
+            pixels.push(green);
+            pixels.push(32);
+            pixels.push(255);
+        }
+    }
+    writer.write_image_data(&pixels).unwrap();
 }
